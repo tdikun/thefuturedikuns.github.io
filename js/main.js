d157041752b848e8f1b53a7b60e980906334e8f1
Tim Dikun <tdikun@gmail.com>
2012-12-21 09:39:58 -0800

    update for save the dates

---
+++ js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+  $('.lightbox_trigger').click(function(e) {
+    e.preventDefault();
+    $('#lightbox').show();
+  });
+
+  $('#lightbox').click(function() {
+    $(this).hide();
+  });
+});
