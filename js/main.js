790a9fc0fea2121258e0612de861038ae02697df
Tim Dikun <tdikun@gmail.com>
2013-02-11 16:20:19 -0800

    update registry

--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
 jQuery(document).ready(function($) {$('.lightbox_trigger').click(function(e) {e.preventDefault();$('#lightbox').show();});$('#lightbox').click(function() {$(this).hide();});});
+jQuery(document).ready(function($) {$('.lightbox_trigger2').click(function(e) {e.preventDefault();$('#lightbox2').show();});$('#lightbox2').click(function() {$(this).hide();});});
