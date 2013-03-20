35e428b3c43a7d1a3bcc26b347eb142c2d624076
Tim Dikun <tdikun@gmail.com>
2013-03-19 21:31:13 -0400

    add shell for weekend

---
+++ weekend.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<head>
+  <title>Wedding Weekend | The Future Dikuns</title>
+  <!--
+  /* @license
+   * MyFonts Webfont Build ID 2509230, 2013-03-19T20:46:47-0400
+   *
+   * The fonts listed in this notice are subject to the End User License
+   * Agreement(s) entered into by the website owner. All other parties are
+   * explicitly restricted from using the Licensed Webfonts(s).
+   *
+   * You may obtain a valid license at the URLs below.
+   *
+   * Webfont: News Gothic Condensed by Bitstream
+   * URL: http://www.myfonts.com/fonts/bitstream/news-gothic/condensed/
+   *
+   * Webfont: News Gothic Bold Condensed by Bitstream
+   * URL: http://www.myfonts.com/fonts/bitstream/news-gothic/bold-condensed/
+   *
+   *
+   * License: http://www.myfonts.com/viewlicense?type=web&buildid=2509230
+   * Webfonts copyright: Copyright 1990-2003 Bitstream Inc. All rights reserved.
+   *
+   * © 2013 MyFonts Inc
+  */
+
+  -->
+
+  <script type="text/javascript">
+  //uncomment and change this to false if you're having trouble with WOFFs
+  //var woffEnabled = true;
+  //to place your webfonts in a custom directory
+  //uncomment this and set it to where your webfonts are.
+  //var customPath = "/themes/fonts";
+  </script>
+  <script type="text/javascript" src="js/MyFontsWebfontsKit.js"></script>
+  <link href="css/weekend.css" media="screen" rel="stylesheet" type="text/css">
+  <script src="js/jquery-1.8.3.min.js"></script>
+  <!--[if lte ie 8]>
+    <style type="text/css">
+
+    </style>
+  <![endif]-->
+
+</head>
+<body>
+  <img src="images/hero.jpg" id="hero" />
+  <div id="wrap">
+    <h1>Content</h1>
+    <div id="content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h2>Bhead style</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  </div>
+  <div id="footer">
+    <a href="http://www.thefuturedikuns.com"></a>
+  </div>
+</body>
+</html>
