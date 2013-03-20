21c97b6f3f42a20feb4ca3be7e467c46b3361b43
Tim Dikun <tdikun@gmail.com>
2013-03-19 23:27:33 -0400

    add weekend content

--- weekend.php
+++ weekend.php
@@ -46,13 +46,130 @@
 <body>
   <img src="images/hero.jpg" id="hero" />
   <div id="wrap">
-    <h1>Content</h1>
+    <h1>Weekend Details</h1>
     <div id="content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <h2>Bhead style</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p id="intro">We're so blessed to have you join us for this wonderful weekend. As many of you are traveling into town for this special occasion, we want you to be prepared with itinerary, contact information and local resources. Please don't hesitate to contact us if you have any questions about the information below. Looking forward to seeing you all April 12th.</p>
+      <h2>Friday, April 12</h2>
+        <ul>
+          <li>11:15 AM&mdash;Depart for Rehearsal from South Shore Harbour Resort</li>
+    <!-- *<br /> <em>Out of town guests requiring transportation may contact</em></li> -->
+          <li>12:00 AM&mdash;Wedding Rehearsal at Oak Wind Bed and Breakfast<br />
+            <em>All wedding attendants and ceremony participants: Please be at the Venue no later than noon.</em></li>
+          <li>1:00 PM&mdash;Rehearsal Lunch at Oak Wind Bed and Breakfast<br />
+            <em>There will be a burger bar for your dining pleasure.</em></li>
+        </ul>
+      <h2>Saturday, April 13</h2>
+        <ul>
+          <li>11:00 AM&mdash;Bridesmaids at Oak Wind Bed and Breakfast</li>
+          <li>3:00 PM&mdash;Gromsmen at Oak Wind Bed and Breakfast</li>
+          <li>3:30 PM&mdash;Photographer at Oak Wind Bed and Breakfast</li>
+          <li>4:30 PM&mdash;Blessing the Bride, Family and Bridesmaids</li>
+          <li>4:30 PM&mdash;Blessing the Groom, Family and Groomsmen</li>
+          <li>5:30 PM&mdash;Wedding Ceremony begins</li>
+        </ul>
+      <h2>Contact Information</h2>
+        <ul id="contact">
+          <li><strong>Ann Rieves</strong><br /> Wedding Coordinator at Oakwind</li>
+          <li>281-386-6540 | <a href="mailto:info@oakwindweddings.com">info@oakwindweddings.com</a></li>
+          <li>&nbsp;</li>
+          <li><strong>Bruce Wesley</strong><br /> Father of the Bride</li>
+          <li>832-221-0440 | <a href="mailto:bwesley@clearcreek.org">bwesley@clearcreek.org</a></li>
+          <li>&nbsp;</li>
+          <li><strong>Susan Wesley</strong><br /> Mother of the Bride</li>
+          <li>281-455-5031 | <a href="mailto:swesley@clearcreek.org">swesley@clearcreek.org</a></li>
+          <li>&nbsp;</li>
+          <li><strong>Alex Dikun</strong><br /> Father of the Groom</li>
+          <li>425-350-1059</li>
+          <li>&nbsp;</li>
+          <li><strong>Cheryl Dikun</strong><br /> Mother of the Groom</li>
+          <li>425-273-1830</li>
+          <li>&nbsp;</li>
+          <li><strong>Micah Wesley</strong><br /> Maid of Honor, sister of the Bride</li>
+          <li>281-974-8384 | <a href="mailto:micah_wesley@yahoo.com">micah_wesley@yahoo.com</a></li>
+          <li>&nbsp;</li>
+          <li><strong>Sarah Gadsby</strong><br /> Bridesmaid, childhood best friend</li>
+          <li>832-277-6114</li>
+          <li>&nbsp;</li>
+          <li><strong>Ryan Hamm</strong><br /> Groomsman, old roommate/friend from Florida</li>
+          <li>630-890-7311</li>
+          <li>&nbsp;</li>
+          <li><strong>Please contact the bride and groom only up until the Thursday before the wedding.</strong></li>
+          <li>&nbsp;</li>
+          <li><strong>Sarahbeth Wesley</strong></li>
+          <li>281-468-5156 | <a href="mailto:sewesley@gmail.com">sewesley@gmail.com</a></li>
+          <li>&nbsp;</li>
+          <li><strong>Tim Dikun</strong></li>
+          <li>425-772-8011 | <a href="mailto:tdikun@gmail.com">tdikun@gmail.com</a></li>
+        </ul>
+      <h2>Wedding Party</h2>
+        <ul>
+          <li>Yancey Arrington&mdash;Officiate</li>
+          <li>&nbsp;</li>
+          <li>Alastair Sterne&mdash;Best Man</li>
+          <li>Andy Overland&mdash;Best Man</li>
+          <li>Brent Strandy&mdash;Groomsman</li>
+          <li>Ryan Hamm&mdash;Groomsman</li>
+          <li>Matt Larson&mdash;Groomsman</li>
+          <li>Lars Coleman&mdash;Groomsman</li>
+          <li>&nbsp;</li>
+          <li>Rebekah Madrid&mdash;Matron of Honor</li>
+          <li>Micah Wesley&mdash;Maid of Honor</li>
+          <li>Marisa Pitman&mdash;Bridesmaid</li>
+          <li>Sarah Gadsby&mdash;Bridesmaid</li>
+          <li>Katy Caulfield&mdash;Bridesmaid</li>
+          <li>Julia Sterne&mdash;Bridesmaid</li>
+          <li>&nbsp;</li>
+          <li>Sofia Strandy&mdash;Flower girl</li>
+          <li>Emmalyn Strandy&mdash;Flower girl</li>
+        </ul>
+      <h2>Wedding Hotel</h2>
+        <p>South Shore Harbour & Conference Center <br />
+          2500 South Shore Blvd <br />
+          League City, TX 77573 | 800-442-5005</p>
+      <h2>Wedding Venue</h2>
+        <p>Oak Wind Bed and Breakfast <br />
+          4601 County Road 156 <br />
+          Alvin, TX  77511 | (281) 585-0060</p>
+        <ul id="directions">
+          <li><strong>From South Shore Hotel in League City  <br />
+            <em>(41 minutes without traffic)</em></strong></li>
+          <li>Right on Marina Bay Drive <em>(the main road)</em></li>
+          <li>Left on S Egret Bay Blvd <em>(FM 270) At Schlotzky's</em></li>
+          <li>Right on FM 646 <em>(Crossover I-45)</em></li>
+          <li>Right at FM 517 <em>(Crowder's Funeral Home on Left)</em></li>
+          <li>Left on Alvin Bypass <em>(I-35 South in Alvin)</em></li>
+          <li>Slight Left on Fairway Drive <em>(County Road 369)</em></li>
+          <li>Veer Left on W Timberlane Drive <em>(You're in a neighborhood)</em></li>
+          <li>Take first right on County Road 156 <em>(Main Street)</em></li>
+          <li>Continue to follow County Road 156, Venue on the Left</em></li>
+          <li>&nbsp;</li>
+          <li><strong>From Downtown Houston  <br />
+            <em>(50 minutes without traffic)</em></strong></li>
+          <li>288 South</li>
+          <li>Take TX 6 exit toward Alvin <em>(follow TX 6 to left)</em></li>
+          <li>Right on Alvin Bypass <em>(I-35 South in Alvin)</em></li>
+          <li>Slight Left on Fairway Drive <em>(County Road 369)</em></li>
+          <li>Veer Left on W Timberlane Drive</li>
+          <li>Take first right on County Road 156 <em>(Main Street)</em></li>
+          <li>Continue to follow County Road 156, Venue on the Left</li>
+        </ul>
+      <h2>From South Shore Harbour Resort</h2>
+        <ul>
+          <li><strong>CVS</strong><br />
+            <em>(3.4 Miles)</em></li>
+          <li>2700 W Main St<br />
+          League City, TX 77573<br />
+          281-332-7430</li>
+          <li>&nbsp;</li>
+          <li><strong>Randalls Super Market</strong><br />
+            <em>(Across the intersection from South Shore Harbour Resort)</em></li>
+          <li>2951 Marina Bay Dr<br />
+          League City, TX 77573<br />
+          281-538-2500</li>
+          <li>&nbsp;</li>
+          <li><strong>Starbucks</strong><br />
+            <em>(Across the street from Randalls)</em></li>
+        </ul>
     </div>
   </div>
   <div id="footer">
